485fe6f2554da23d811064e74da08dc301a852de
Will Hodgson <william.hodgson.mail@gmail.com>
2016-05-02 00:50:35 +0100

    Refactoring
    + Split name generation components into separate functions.

--- PartNameGenerator.py
+++ PartNameGenerator.py
@@ -7,30 +7,37 @@ class PartNameGenerator:
     def __init__(self,manufacturerName,typeName):
         self.manufacturerName = manufacturerName
         self.typeName = typeName
+        
     def generateName(self):
-        partNameSerial = ""
-        partNameSerialLength = 3
-        partNameAlias = ""
-        partNameSpec = ""
-        #Generate Random Serial
-        partNameSerial = self.manufacturerName[0] + self.typeName[0] + "-" \
-        + self.generateSerial(partNameSerialLength)
-        #Get Random Alias if File Exists
-        if os.path.isfile("res/" + self.typeName + "Alias.txt"):
-            with open("res/" + self.typeName + "Alias.txt") as file:
-                lines = file.read().splitlines()
-            partNameAlias = random.choice(lines)
-        #Get Rnadom Spec if File Exists
-        if os.path.isfile("res/" + self.typeName + "Spec.txt"):
-            with open("res/" + self.typeName + "Spec.txt") as file:
-                lines = file.read().splitlines()
-            partNameSpec = random.choice(lines)
-        else:
-            partNameSpec = "Part"
+        partNameSerial = self.generateSerial(self.manufacturerName,self.typeName,3)
+        partNameAlias = self.getAlias(self.typeName)
+        partNameSpec = self.getSpec(self.typeName)
         #Return Generated Part Name
-        return partNameSerial + " '" + partNameAlias + "' " + partNameSpec
+        if(partNameAlias != ""):
+            return partNameSerial + " " + partNameAlias + " " + partNameSpec
+        else:
+            return partNameSerial + " " + partNameSpec
     
-    def generateSerial(self,size):
+    def generateAlphanumeric(self,size):
         chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         return "".join(random.choice(chars) for _ in range(size))
     
+    def generateSerial(self,manufacturerName,typeName,size):
+        return manufacturerName[0] + typeName[0] + "-" \
+        + self.generateAlphanumeric(size)
+        
+    def getAlias(self,typeName):
+        if os.path.isfile("res/" + typeName + "Alias.txt"):
+            with open("res/" + typeName + "Alias.txt") as file:
+                lines = file.read().splitlines()
+            return "'" + random.choice(lines) + "'"
+        else:
+            return ""
+        
+    def getSpec(self,typeName):
+        if os.path.isfile("res/" + typeName + "Spec.txt"):
+            with open("res/" + typeName + "Spec.txt") as file:
+                lines = file.read().splitlines()
+            return random.choice(lines)
+        else:
+            return "Part"
